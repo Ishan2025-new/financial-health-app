209ac22381ed86f36283b6b93659bb37bc068f53
ishangithub2024 <rudrashis.chowdhury@gmail.com>
2025-05-13 10:50:03 +0530

    major project

--- app.py
+++ app.py
@@ -113,4 +113,4 @@ def logout():
     return redirect(url_for('admin_login'))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+      app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
